06df5f6e141b47b089d854ccdce1827e5125f4c2
Gabriel Alves <gabriel52.alves@gmail.com>
2025-02-05 17:22:40 -0300

    fix: fixing the Total por Categoria endpoint logic

--- core/views/categoria.py
+++ core/views/categoria.py
@@ -1,3 +1,4 @@
+import stat
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
@@ -5,9 +6,11 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from django.db.models.aggregates import Sum
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Count
 
 from core.models import Categoria
 from core.serializers import CategoriaSerializer
+
 
 class CategoriaViewSet(ModelViewSet):
     """ViewSet for categorias."""
@@ -20,11 +23,10 @@ class CategoriaViewSet(ModelViewSet):
 
     def get_queryset(self):
         usuario = self.request.user
-        if usuario.is_superuser:
+        if usuario.is_superuser or usuario.groups.filter(name="admin"):
             return Categoria.objects.all()
-        if usuario.groups.filter(name="admin"):
-            return Categoria.objects.all()
-        return Categoria.objects.filter(usuario=usuario)
+        else:
+            return Categoria.objects.filter(usuario=usuario)
 
     def perform_create(self, serializer):
         """Associate the user with the categoria."""
@@ -32,10 +34,12 @@ class CategoriaViewSet(ModelViewSet):
 
     @action(detail=False, methods=["get"])
     def total_por_categoria(self, request):
-        """Return the total of categorias per usuario."""
-        total_por_categoria = (
-            Categoria.objects.values("usuario__username")
-            .annotate(total=Sum("id"))
-            .order_by("usuario__username")
-        )
-        return Response(total_por_categoria, status=status.HTTP_200_OK)
+        """Return the total of tarefas per categoria for the authenticated user."""
+        usuario = request.user
+        if usuario.is_superuser or usuario.groups.filter(name="admin").exists():
+            categorias = Categoria.objects.all()
+        else:
+            categorias = Categoria.objects.filter(usuario=usuario)
+
+        total = categorias.annotate(total_tarefas=Count("tarefas")).values("nome", "total_tarefas")
+        return Response(total, status=status.HTTP_200_OK)
