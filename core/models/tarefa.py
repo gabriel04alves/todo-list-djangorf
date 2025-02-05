bb1328d6c40a05c96026ef1ba75a2860bba6fdff
Gabriel Alves <gabriel52.alves@gmail.com>
2025-02-05 19:52:06 -0300

    refactor: update Tarefa model to use ManyToMany relationship for categorias

--- core/models/tarefa.py
+++ core/models/tarefa.py
@@ -3,19 +3,20 @@ from .user import User
 from .categoria import Categoria
 import datetime
 
+
 class Tarefa(models.Model):
     class StatusTarefa(models.TextChoices):
         PENDENTE = "pendente"
         EM_ANDAMENTO = "em_andamento"
         CONCLUIDA = "concluida"
-    
+
     titulo = models.CharField(max_length=255)
     descricao = models.TextField(blank=True)
-    status = models.CharField(max_length=15,choices=StatusTarefa.choices, default=StatusTarefa.PENDENTE)
+    status = models.CharField(max_length=15, choices=StatusTarefa.choices, default=StatusTarefa.PENDENTE)
     prazo = models.DateField()
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name="tarefas")
+    categorias = models.ManyToManyField(Categoria, related_name="tarefas")
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="tarefas")
     criado_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.titulo} | {self.categoria}"
+        return f"{self.titulo} | {', '.join([categoria.nome for categoria in self.categorias.all()])}"
